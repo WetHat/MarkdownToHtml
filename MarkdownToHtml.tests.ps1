4c20faac72af2fef4cfa28fbd9928d89108e5a6d
WetHat <PeterErnst@t-online.de>
2019-09-14 20:02:50 +0200

    Issue #2 Tests reorganized and new HTML fragment tests added

--- MarkdownToHtml.tests.ps1
+++ MarkdownToHtml.tests.ps1
@@ -12,12 +12,12 @@
 Describe 'Convert-MarkdownToHTML' {
 	It 'Converts markdown file(s) from ''<Path>'' to HTML' `
 	   -TestCases @(
-		   @{Path='markdown/mermaid.md'; ReferencePath='mermaid.html';       ResultPath='TestDrive:/mermaid.html' ;             Extensions = 'diagrams'}
-		   @{Path='markdown/KaTex.md';   ReferencePath='KaTex.html';         ResultPath='TestDrive:/KaTex.html' ;               Extensions = 'mathematics'}
-		   @{Path='markdown/KaMaid.md';  ReferencePath='KaMaid.html';        ResultPath='TestDrive:/KaMaid.html' ;              Extensions = 'diagrams','mathematics'}
-		   @{Path='markdown/Code.md';    ReferencePath='Code.html';          ResultPath='TestDrive:/Code.html' ;                Extensions = 'advanced'}
-		   @{Path='markdown/Dir';        ReferencePath='SubDir/SubDirSingle.html';  ResultPath='TestDrive:/Subdir/SubdirSingle.html' ; Extensions = 'advanced'}
-		   @{Path='markdown/Dir2';       ReferencePath='SubDir2/SubDir2/SubDirSingle.html';  ResultPath='TestDrive:/Subdir2/SubDir2/SubdirSingle.html' ; Extensions = 'advanced'}
+		   @{Path='markdown/mermaid.md'; ReferencePath='html/mermaid.html';       ResultPath='TestDrive:/mermaid.html' ;             Extensions = 'diagrams'}
+		   @{Path='markdown/KaTex.md';   ReferencePath='html/KaTex.html';         ResultPath='TestDrive:/KaTex.html' ;               Extensions = 'mathematics'}
+		   @{Path='markdown/KaMaid.md';  ReferencePath='html/KaMaid.html';        ResultPath='TestDrive:/KaMaid.html' ;              Extensions = 'diagrams','mathematics'}
+		   @{Path='markdown/Code.md';    ReferencePath='html/Code.html';          ResultPath='TestDrive:/Code.html' ;                Extensions = 'advanced'}
+		   @{Path='markdown/Dir';        ReferencePath='html/SubDir/SubDirSingle.html';  ResultPath='TestDrive:/Subdir/SubdirSingle.html' ; Extensions = 'advanced'}
+		   @{Path='markdown/Dir2';       ReferencePath='html/SubDir2/SubDir2/SubDirSingle.html';  ResultPath='TestDrive:/Subdir2/SubDir2/SubdirSingle.html' ; Extensions = 'advanced'}
 	   ) `
 	   {
 		   param($Path,$ReferencePath,$ResultPath,$Extensions)
@@ -39,4 +39,36 @@ Describe 'Convert-MarkdownToHTML' {
 		   $refFileContents = Get-Content -LiteralPath $refPath -Encoding UTF8 | Out-String
 		   Get-Content -LiteralPath $ResultPath -Encoding UTF8 | Out-String | Should -BeExactly $refFileContents
 	   }
+}
+
+Describe 'Convert-MarkdownToHTMLFragment' {
+	It 'Converts markdown file(s) from ''<Path>'' to HTMLFragments' `
+		-TestCases @(
+		   @{Path='markdown/mermaid.md'; ReferencePath='mermaid.html'; Extensions = 'diagrams' ; Title = 'Mermaid Diagramming Tool'}
+		   @{Path='markdown/KaTex.md';   ReferencePath='KaTex.html';   Extensions = 'mathematics'; Title = 'KaTEx Typesetting'}
+		   @{Path='markdown/KaMaid.md';  ReferencePath='KaMaid.html';  Extensions = 'diagrams','mathematics'; Title = 'KaTEx Typesetting'}
+		   @{Path='markdown/Code.md';    ReferencePath='Code.html';    Extensions = 'advanced'; Title = 'Testing Code Symtax Highlighting'}
+		) `
+		{
+			param($Path,$ReferencePath,$Extensions, $Title)
+
+			[System.IO.FileInfo]$testPath = Join-Path $SCRIPT:testdata -ChildPath $Path
+			$refPath  = Join-Path $SCRIPT:refdata  -ChildPath $ReferencePath
+
+			$ResultPath = Join-Path 'TestDrive:/' $ReferencePath
+			$ResultPath = Join-Path 'e:\temp\ttt' $ReferencePath
+
+			$fragment = Get-Item -LiteralPath $testPath `
+			| Convert-MarkdownToHTMLFragment -IncludeExtension $Extensions
+		
+			$fragment.RelativePath | Should -BeExactly $testPath.Name # Should be Markdown Filename
+			$fragment.Title
+
+			Out-File -InputObject $fragment.HtmlFragment -LiteralPath $ResultPath -Encoding utf8
+
+			$refPath    | Should -Exist
+			$ResultPath | Should -Exist
+			$refFileContents = Get-Content -LiteralPath $refPath -Encoding UTF8 | Out-String
+			Get-Content -LiteralPath $ResultPath -Encoding UTF8 | Out-String | Should -BeExactly $refFileContents
+		}
 }
