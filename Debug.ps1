470e0b82f56e6210e8044885bf3ecc746450fb39
WetHat <PeterErnst@t-online.de>
2022-01-17 17:51:45 +0100

    module imported from local directory

--- Debug.ps1
+++ Debug.ps1
@@ -1,9 +1,9 @@
-param($TestName)
+param($TestName='ConvertTo-PageHeadingNavigation')
 
 [System.IO.DirectoryInfo]$SCRIPT:moduleDir = Split-Path -Parent $MyInvocation.MyCommand.Path
 $saved = $env:PSModulePath
 $env:PSModulePath = "$($SCRIPT:moduleDir.Parent.Fullname);" + $env:PSModulePath
 
-import-module MarkdownToHTML -force
+import-module -name './' -force
 Invoke-Pester -FullNameFilter $TestName -Verbose
 $env:PSModulePath = $saved
